b30897ea4dc3a9af3c09d24f17f0128854a0615e
STUDENT in 40924 <dmitri.mcguckin@syccuxas01.pcc.edu>
2017-12-02 07:53:11 -0800

    Added Pipes and FD's 1.0.0

--- a03.c
+++ a03.c
@@ -8,43 +8,41 @@
 // Parent process prints sum of numbers.
 
 #include <stdio.h>
-#include <sys/types.h>
+
+#define NOHANG 0
 
 int main(int argc, char **argv)
 {
+	int status;
+	int pid;
 	int list[4];
-	int pipe1[2];
-	int pipe2[2];
+	int toPipe[2];
+	int fromPipe[2];
 	int num = argc - 2;
 
 	for(int i = 0; i < num; i++)
 	{
 		list[i] = atoi(argv[i+1]);
-		//printf("%i ",list[i]);
 	}
-	//printf("\n");
 
 	// set up pipe
-	pipe(pipe1);
-	pipe(pipe2);
+	pipe(toPipe);
+	pipe(fromPipe);
 
 	//printf("Pipe 1: %i, %i\nPipe 2: %i, %i\n", pipe1[0], pipe1[1], pipe2[0], pipe2[1]);
 
-	// call fork()
-	int pid = fork();
-
+	system("clear");
 	printf("CS201 - Assignment 3 Regular - Dmitri McGuckin\n");
+
+	pid = fork();
 
 	if (pid == 0)
 	{
 			// -- running in child process --
 			int     sum = 0;
 
-			printf("Congratulations! You're in the child process now!\n");
-			dup2(pipe1[1],STDOUT_FILENO);
-			close(pipe1[1]);
-			close(pipe1[0]);
-			dup2(pipe2[0],STDIN_FILENO);
+			printf("PARENT PID: %i\n", pid);
+			
 			// Receive characters from parent process via pipe
 			// one at a time, and count them.
 
@@ -55,14 +53,24 @@ int main(int argc, char **argv)
 	{
 			// -- running in parent process --
 			int sum = 0;
-
+			
+			printf("CHILD PID: %i\n", pid);
+			
 			// Send numbers (datatype: int, 4 bytes) from command line arguments
 			// starting with argv[1] one at a time through pipe to child process.
+			
+			for(int i = 0; i < num; i++)
+			{
+				write(toPipe,&list[i],4);
+				printf("Written to pipe: %i\n",list[i]);
+			}	
 
 			// Wait for child process to return. Reap child process.
 			// Receive sum of numbers via the value returned when
 			// the child process is reaped.
 
+			waitpid(pid,status,NOHANG);
+			
 			printf("sum = %d\n", sum);
 			return 0;
 		}
