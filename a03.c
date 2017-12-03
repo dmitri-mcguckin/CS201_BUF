9497a98ce2a7bb340d656498e88713d5c0356d5d
dmitri.mcguckin <dmitri.mcguckin26@gmail.com>
2017-12-02 17:09:31 -0800

    relayed pipe boundaries

--- a03.c
+++ a03.c
@@ -15,15 +15,9 @@ int main(int argc, char **argv)
 {
 	int status;
 	int pid;
-	int list[4];
 	int toPipe[2];
 	int fromPipe[2];
-	int num = argc - 2;
-
-	for(int i = 0; i < num; i++)
-	{
-		list[i] = atoi(argv[i+1]);
-	}
+	int in, out;
 
 	// set up pipe
 	pipe(toPipe);
@@ -39,38 +33,47 @@ int main(int argc, char **argv)
 	if (pid == 0)
 	{
 			// -- running in child process --
-			int     sum = 0;
+			int sum = 0;
 
 			printf("PARENT PID: %i\n", pid);
-			
+			close(fromPipe[0]);
+			out = fromPipe[1];
+			in = toPipe[0];
+			close(toPipe[1]);
+
 			// Receive characters from parent process via pipe
 			// one at a time, and count them.
+			char ch;
+
+			for (int i = 0; i < 4; i++)
+			{
+				read(in, &ch, 1);
+				sum = sum + list[i];
+			}
+
 
 			// Return sum of numbers.
+			printf("child sum = %d\n", sum);
 			return sum;
 	}
 	else
 	{
 			// -- running in parent process --
 			int sum = 0;
-			
+
 			printf("CHILD PID: %i\n", pid);
-			
+
 			// Send numbers (datatype: int, 4 bytes) from command line arguments
 			// starting with argv[1] one at a time through pipe to child process.
-			
-			for(int i = 0; i < num; i++)
-			{
-				write(toPipe,&list[i],4);
-				printf("Written to pipe: %i\n",list[i]);
-			}	
 
 			// Wait for child process to return. Reap child process.
 			// Receive sum of numbers via the value returned when
 			// the child process is reaped.
 
 			waitpid(pid,status,NOHANG);
-			
+
+
+
 			printf("sum = %d\n", sum);
 			return 0;
 		}
